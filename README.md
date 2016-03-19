9f10f051010bdcc780bd8c1ef6a2b236a2f9bd82
nfischer <nadav.fischer@capriza.com>
2016-03-19 13:17:14 +0200

    the app file

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gmail-app
+# Gmail OSX App
+
 Gmail OSX app based on Electron
+
+* Dock notifications with unread emails count
+* Desktop popup notifications
+* Familiar web Gmail interface
+* Based on [Electron](http://electron.atom.io/)
+
+## How to Build
+
+```
+clone https://github.com/fujifish/gmail-app.git
+cd gmail-app
+npm install
+npm run build
+```
+
+Copy the built application `Gmail.app` from in `Gmail-darwin-x64` to your Applications directory.
+
+## License
+
+The MIT license. Copyright 2016 fujifish.
---
+++ main.js
@@ -0,0 +1,196 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var Menu = require('menu');
+var Tray = require('tray');
+var util = require('util');
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var window = null;
+var tray = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // if (process.platform != 'darwin')
+    app.quit();
+});
+
+function createGmailWindow(url) {
+  // Create the browser window.
+  window = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    title: 'Gmail',
+    icon: __dirname+'/gmail-icon-unread.png',
+    "skip-taskbar": process.platform !== 'darwin'
+  });
+
+  window.maximize();
+
+  // mainWindow.loadUrl('https://mail.google.com/');
+  window.loadUrl('file://'+__dirname+'/index.html');
+
+  // Emitted when the window is closed.
+  window.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    window = null;
+  });
+
+  window.on('minimize', function(event) {
+    window.hide();
+    event.preventDefault();
+  });
+}
+
+function setupTray() {
+  tray = new Tray(__dirname+'/gmail-icon-default.png');
+  tray.setToolTip('No unread messages');
+  var trayContextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show Inbox',
+      click: function() { window.show(); }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit',
+      click: function() { app.quit(); }
+    },
+  ]);
+  tray.setContextMenu(trayContextMenu);
+
+  tray.on('clicked', function() {
+    window.show();
+  });
+}
+
+function setupMenu() {
+  var template = [
+    {
+      label: 'Gmail',
+      submenu: [
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() { app.quit(); }
+        },
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'Command+Z',
+          selector: 'undo:'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+Command+Z',
+          selector: 'redo:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'Command+X',
+          selector: 'cut:'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'Command+C',
+          selector: 'copy:'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'Command+V',
+          selector: 'paste:'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Find',
+          accelerator: 'Command+F',
+          click: function() { BrowserWindow.getFocusedWindow().webContents.send('find'); }
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'Command+R',
+          click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+        },
+        {
+          label: 'Toggle DevTools',
+          accelerator: 'Alt+Command+I',
+          click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'Command+M',
+          selector: 'performMiniaturize:'
+        },
+        {
+          label: 'Close',
+          accelerator: 'Command+W',
+          selector: 'performClose:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          selector: 'arrangeInFront:'
+        },
+      ]
+    },
+  ];
+
+  menu = Menu.buildFromTemplate(template);
+
+  Menu.setApplicationMenu(menu); // Must be called within app.on('ready', function(){ ... });
+}
+
+// This method will be called when atom-shell has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+
+  var ipc = require('ipc');
+  ipc.on('add-account', function(event, args) {
+      createGmailWindow(args);
+  });
+
+  ipc.on('unread', function(event, unread) {
+    app.dock && app.dock.setBadge(unread);
+    tray.setImage(unread.length === 0 ? __dirname+'/gmail-icon-default.png' : __dirname+'/gmail-icon-unread.png');
+    tray.setToolTip((unread.length === 0 ? 'No': unread ) + ' unread messages');
+  });
+
+  createGmailWindow('file://'+__dirname+'/index.html');
+
+  setupTray();
+
+  setupMenu();
+
+});
