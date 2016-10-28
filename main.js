4f3049b8dc6bbf753df2831244dd46c3183f1bee
fujifish <fujifischer@gmail.com>
2016-10-28 10:19:53 +0300

    fix reloading with CMD+R

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ function setupMenu() {
         {
           label: 'Reload',
           accelerator: 'Command+R',
-          click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+          click: function() { BrowserWindow.getFocusedWindow().webContents.reloadIgnoringCache(); }
         },
         {
           label: 'Toggle DevTools',
